0a45a42916438c8874df1303a98012c25422acd4
計良 風太 <kera@allianceport.jp>
2015-10-20 16:24:20 +0900

    クリックファンクションを減らして、app.jsがsettings,jsを読み込むように改善しました。

--- app.js
+++ app.js
@@ -1,7 +1,56 @@
-var http = require('http');
+var http = require("http");
 var fs = require("fs");
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var output = fs.readFileSync("./index.html", "utf-8");
-  res.end(output);
-}).listen(8000);
+var path = require("path");
+var server = http.createServer(requestListener);
+server.listen((8000), function() {
+    console.log((process.env.PORT || 8000));
+});
+function requestListener(request, response) {
+    var requestURL = request.url;
+    var extensionName = path.extname(requestURL);
+    switch(extensionName)
+    {
+        case ".html":
+            readFileHandler(requestURL, "text/html", false, response);
+            break;
+        case ".js":
+            readFileHandler(requestURL, "text/javascript", false, response);
+            break;
+        default:
+            readFileHandler("/index.html", "text/html", false, response);
+            break;
+    }
+}
+
+function readFileHandler(fileName, contentType, isBinary, response) {
+    var encoding = !isBinary ? "utf8" : "binary";
+    var filePath = __dirname + fileName;
+
+    fs.exists(filePath, function(exits) {
+        if(exits)
+        {
+            fs.readFile(filePath, {encoding: encoding}, function (error, data) {
+                if (error) {
+                    response.statusCode = 500;
+                    response.end("Internal Server Error");
+                } else {
+                    response.statusCode = 200;
+                    response.setHeader("Content-Type", contentType);
+                    if(!isBinary)
+                    {
+                        response.end(data);
+                    }
+                    else
+                    {
+                        response.end(data, "binary");
+                    }
+                }
+            });
+        }
+        else
+        {
+            response.statusCode = 400;
+            response.end("400 Error");
+        }
+    });
+}
